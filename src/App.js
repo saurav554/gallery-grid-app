86e39f489aaac9b30d27517bcd15ae08364642f5
saurav <avisaurav485@gmail.com>
2021-06-02 13:24:27 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,15 @@
-import logo from './logo.svg';
-import './App.css';
-
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import React from 'react'
+import NavigationBar from './Components/NavigationBar'
+import Card from './Components/Card'
+import MyButton from './Components/Footer'
+import "./App.css"
+const App =()=>{
+    return (
+        <>
+        <NavigationBar/>
+        <Card/>
+        <MyButton/>
+        </>
+    )
 }
-
-export default App;
+export default App
---
+++ src/Components/tileData.js
@@ -0,0 +1,83 @@
+const tileData = [
+    {
+    img: 'https://images.unsplash.com/photo-1470259078422-826894b933aa?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=753&q=80',
+    title: 'Alyce Bass',
+    },
+    {
+    img: 'https://images.unsplash.com/photo-1502378735452-bc7d86632805?ixlib=rb-0.3.5&q=80&fm=jpg&crop=entropy&cs=tinysrgb&w=200&fit=max&s=aa3a807e1bbdfd4364d1f449eaa96d82',
+    title: 'Sabiha Plummer',
+    featured: true,
+    },
+    {
+      img: 'https://images.unsplash.com/photo-1529626455594-4ff0802cfb7e?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=334&q=80',
+      title: 'Ayo Ogunseinde',
+    },
+    {
+      img: 'https://images.unsplash.com/photo-1513379733131-47fc74b45fc7?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=334&q=80',
+      title: 'Nicole Geri',
+    },
+    {
+      img: 'https://images.unsplash.com/photo-1520813792240-56fc4a3765a7?ixlib=rb-1.2.1&q=80&fm=jpg&crop=entropy&cs=tinysrgb&w=200&fit=max&ixid=eyJhcHBfaWQiOjE3Nzg0fQ',
+      title: 'Cobie Morin',
+    },
+    {
+      img: 'https://uifaces.co/our-content/donated/KtCFjlD4.jpg',
+      title: 'Arthur Stewart',
+      featured: true,
+    },
+    {
+      img: 'https://uifaces.co/our-content/donated/n4Ngwvi7.jpg',
+      title: 'Keanu Hood',
+    },
+    {
+      img: 'https://images.unsplash.com/photo-1581338834647-b0fb40704e21?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=334&q=80',
+      title: 'Radu Florin',
+    },
+    {
+      img: 'https://images.pexels.com/photos/1394499/pexels-photo-1394499.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500',
+      title: 'Reema Daniel',
+    },
+    {
+      img: 'https://images.unsplash.com/photo-1620781323455-2afd62a4f8f5?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1400&q=80',
+      title: 'Brock Wegner',
+    },
+    {
+      img: 'https://images.pexels.com/photos/774909/pexels-photo-774909.jpeg?auto=compress&cs=tinysrgb&h=350',
+      title: 'Corin Mcghee',
+    },
+    {
+      img: 'https://images.pexels.com/photos/1858175/pexels-photo-1858175.jpeg?auto=compress&cs=tinysrgb&h=350',
+      title: 'Ali Hendricks',
+    },
+    {
+      img: 'https://images.unsplash.com/photo-1456327102063-fb5054efe647?ixlib=rb-0.3.5&q=80&fm=jpg&crop=entropy&cs=tinysrgb&w=200&fit=max&s=f05c14dd4db49f08a789e6449604c490',
+      title: 'Atticus Aldred',
+    },
+    {
+      img: 'https://images.unsplash.com/photo-1622071285730-f05bd192d69d?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=400&q=80',
+      title: 'Corin Mcghee',
+    },
+    {
+      img: 'https://images.unsplash.com/photo-1496360166961-10a51d5f367a?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=334&q=80',
+      title: 'Ali Hendricks',
+    },
+    {
+      img: 'https://images.unsplash.com/photo-1621036619908-ccbe160a0cdb?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1400&q=80',
+      title: 'Elise Wilcox',
+    },
+    {
+      img: 'https://images.pexels.com/photos/774909/pexels-photo-774909.jpeg?auto=compress&cs=tinysrgb&h=350',
+      title: 'Corin Mcghee',
+    },
+    {
+      img: 'https://images.unsplash.com/photo-1622165358807-6347ca836610?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=400&q=80',
+      title: 'Ali Choubin',
+    },
+
+
+
+
+
+  ];
+  
+  export default tileData;
